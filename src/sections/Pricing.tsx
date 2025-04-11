df34f1998cc7473853ebdcce1bf987a69b5cc017
Syed Muhammad Murtaza <smmhj19@gmail.com>
2025-04-12 00:01:56 +0500

    element and key prop changed

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -130,7 +130,7 @@ export const Pricing = () => {
                   {buttonText}
                 </button>
                 <ul className="flex flex-col gap-5 mt-8">
-                  {features.map((index,feature) => (
+                  {features.map((feature,index) => (
                     <li key={index} className="text-sm flex items-center gap-4">
                       <CheckIcon className="h-6 w-6" />
                       <span>{feature}</span>
